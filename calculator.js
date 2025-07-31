4e591164a592aa7e6f4af239f449e1333cd8ce75
animaaryan <aaryan.junnarkar@gmail.com>
2025-07-31 17:10:32 +0530

    Starting point

---
+++ calculator.js
@@ -0,0 +1,25 @@
+// A calculator operation will consist of a number, an operator, and another number
+const NUMBER = 0;
+const OPERATOR = ['+', '-', '/', '*'];
+const NUMBER_02 = 0;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(oper, num1, num2) {
+
+
+}
