4a0f300d3b1d484c8763c7f6c83ade67965f960b
animaaryan <aaryan.junnarkar@gmail.com>
2025-08-01 14:22:35 +0530

    Calculator works on user prompt now

--- calculator.js
+++ calculator.js
@@ -21,5 +21,25 @@ function divide(a, b) {
 
 function operate(oper, num1, num2) {
 
+    num1 = prompt("Enter a number: ");
+    num2 = prompt("Enter another number: ");
+    oper = prompt("Enter a calculation symbol: ");
 
+    if (oper == OPERATOR[0]) {
+        return add(num1, num2);
+    }
+    else if (oper == OPERATOR[1]) {
+        return subtract(num1, num2);
+    }
+    else if (oper == OPERATOR[2]) {
+        return divide(num1, num2);
+    }
+    else if (oper == OPERATOR[3]) {
+        return multiply(num1, num2);
+    }
+    else {
+        console.log("Incorrect choice!");
+    }
 }
+
+console.log(operate(OPERATOR, NUMBER, NUMBER_02));
