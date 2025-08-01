626ae42129ab9f2b5c4304f9024949856b29f6d0
animaaryan <aaryan.junnarkar@gmail.com>
2025-08-01 17:17:18 +0530

    Designed the calculator UI :3

--- calculator.js
+++ calculator.js
@@ -3,43 +3,57 @@ const NUMBER = 0;
 const OPERATOR = ['+', '-', '/', '*'];
 const NUMBER_02 = 0;
 
+// Function to run an addition on two numbers
 function add(a, b) {
     return a + b;
 }
 
+// Function to run a substraction on two numbers
 function subtract(a, b) {
     return a - b;
 }
 
+// Function to run a multiplication on two numbers
 function multiply(a, b) {
     return a * b;
 }
 
+// Function to run a division on two numbers
 function divide(a, b) {
-    return a / b;
+    return Math.round((a / b), 4);
 }
 
+// Depending on what the user calls, the operate function runs
 function operate(oper, num1, num2) {
 
-    num1 = prompt("Enter a number: ");
-    num2 = prompt("Enter another number: ");
-    oper = prompt("Enter a calculation symbol: ");
+    // num1 = prompt("Enter a number: ");
+    // num2 = prompt("Enter another number: ");
+    // oper = prompt("Enter a calculation symbol: ");
 
-    if (oper == OPERATOR[0]) {
-        return add(num1, num2);
-    }
-    else if (oper == OPERATOR[1]) {
-        return subtract(num1, num2);
-    }
-    else if (oper == OPERATOR[2]) {
-        return divide(num1, num2);
-    }
-    else if (oper == OPERATOR[3]) {
-        return multiply(num1, num2);
-    }
-    else {
-        console.log("Incorrect choice!");
+    // Run a switch case to run through the operations depending
+    // on what the user selects
+    switch (oper) {
+
+        //Case add
+        case OPERATOR[0]:
+            return add(num1, num2);
+
+        //Case subtract
+        case OPERATOR[1]:
+            return subtract(num1, num2);
+
+        //Case divide
+        case OPERATOR[2]:
+            return divide(num1, num2);
+
+        // Case multiply
+        case OPERATOR[3]:
+            return multiply(num1, num2);
+
+        // Almost like an else
+        default:
+            console.log("Incorrect choice!");
     }
 }
 
-console.log(operate(OPERATOR, NUMBER, NUMBER_02));
+// alert("Your calculation result is: " + operate(OPERATOR, NUMBER, NUMBER_02));
